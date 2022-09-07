f6afc101ffb9f5348f1492715725d1aca7e7cd1a
Bhavleen Singh <singhbhavleen3@gmail.com>
2022-09-08 01:11:50 +0530

    Upated DTO's

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,46 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
+@Serialize(UserDto)   // No password DTO
 export class UsersController {
-    constructor(private usersService: UsersService) { }
+  constructor(private usersService: UsersService) { }
 
-    @Post("/signup")
-    createrUser(@Body() body: CreateUserDto) {
-        this.usersService.create(body.email, body.password);
+  @Post("/signup")
+  createrUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
+  };
+
+  // Line number 18 and 19 are same, in line 19, we initialized our own decorator
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+  // @Serialize(UserDto)
+  @Get("/:id")
+  async findUser(@Param("id") id: string) {
+    console.log("handler is running");
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException("User not found.")
     }
+    return user;
+  };
+
+  @Get()
+  findAllUsers(@Query("email") email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete("/:id")
+  removeUser(@Param("id") id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch("/:id")
+  updateUser(@Param("id") id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { userInfo } from 'os';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
@@ -10,39 +9,31 @@ export class UsersService {
 
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
-
     return this.repo.save(user);
   };
 
   find(email: string) {
-    // return this.repo.find({ email });
-  }
+    return this.repo.findBy({ email });
+  };
 
   findOne(id: number) {
-    console.log("hey id", id)
-    // return this.repo.findOne(id);
-    return {}
-  }
+    return this.repo.findOneBy({ id });
+  };
 
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
-
     if (!user) {
-      throw new Error("User not found");
+      throw new NotFoundException("User not found");
     }
-
     Object.assign(user, attrs);
     return this.repo.save(user);
-  }
+  };
 
   async remove(id: number) {
     const user = await this.findOne(id);
-
     if (!user) {
-      throw new Error("User not found");
+      throw new NotFoundException("User not found");
     }
-
-    // return this.repo.remove(user);
-
-  }
+    return this.repo.remove(user);
+  };
 }
